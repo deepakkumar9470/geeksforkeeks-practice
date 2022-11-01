f2fe5cd747608ec822cbaaa119b475b53c6e56a7
deepakkumar9470 <dk397787@gmail.com>
2022-10-31 20:04:45 -0400

    gfg-till-31-oct

--- Arrays/app-med.cpp
+++ Arrays/app-med.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <math.h>
 #include <vector>
+#include <limits.h>
 std::vector<int> arr;
 using namespace std;
 
@@ -165,3 +166,192 @@ using namespace std;
 //  }
 
 
+
+/********** Pythagorean Triplet: Given an array arr of N integers, write a function that
+ *  returns true if there is a triplet (a, b, c) that  satisfies a2 + b2 = c2, otherwise false.   **********/
+
+// Amazon , Adobe
+
+// Input:  N = 5
+// Arr[] = {3, 2, 4, 6, 5}
+// Output: Yes
+// Explanation: a=3, b=4, and c=5 forms a
+// pythagorean triplet.
+
+
+// int main(){
+
+//     int arr[] = {3,4,5};
+
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     int a, b, c;
+//     cin>>a>>b>>c;
+   
+//     int max = a;
+
+//     if(b > max) max = b;
+//     if(c> max) max = c;
+
+//     if(max == a){
+//        if( (a*a) == (b*b + c*c)){
+//           cout<<"Yes";
+//        }else{
+//         cout<<"No";
+//        }
+
+//     }else if(max == b){
+//          if( (b*b) == (a*a + c*c)){
+//           cout<<"Yes";
+//        }else{
+//         cout<<"No";
+//        }
+
+//     }else if(max == c){
+//     if( (c*c) == (a*a + b*b)){
+//             cout<<"Yes";
+//         }else{
+//             cout<<"No";
+//        }
+//     }
+
+
+//     return 0 ;
+// }
+
+
+
+
+
+
+
+
+/**********Minimum distance between two numbers:
+ * You are given an array A, of N elements. 
+ * Find minimum index based distance between two elements of the array, x and y.    **********/
+
+// Amazon , Paytm
+
+// Input: N = 4 ,   A[] = {1,2,3,2}
+// x = 1, y = 2
+// Output: 1
+// Explanation: x = 1 and y = 2. There are
+// two distances between x and y, which are
+// 1 and 3 out of which the least is 1.
+
+
+// int main(){
+
+//         int arr[] = {4 ,2 ,1, 5, 3};
+
+//         int n = sizeof(arr) / sizeof(arr[0]);
+        
+//          for (int i = 0; i < n; i++)
+//          {
+//             if(arr[i] >  arr[i+1]){
+//                 arr[i] = arr[i+1];
+
+//             }else{
+//                 arr[i] = -1;
+//             }
+//          }
+         
+//         arr[n-1] = -1;
+         
+//         for (int i = 0; i < n; i++)
+//         {
+//             cout<<arr[i]<<" ";
+//         }
+         
+
+//         return 0;
+// }
+
+
+
+
+/********** Minimum distance between two numbers:
+You are given an array A, of N elements. Find minimum index based distance between two elements of the array, 
+x and y.  **********/
+// Amazon , Paytm
+
+// Input: N = 4
+// A[] = {1,2,3,2}
+// x = 1, y = 2
+// Output: 1
+// Explanation: x = 1 and y = 2. There are
+// two distances between x and y, which are
+// 1 and 3 out of which the least is 1.
+
+
+
+
+// int main(){
+     
+//      int A[] = {1,2,3,2};
+//      int n  = sizeof(arr) / sizeof(arr[0]);
+
+//      int x = 1, y= 2;
+//      int prev = -1; // represent not any data or value
+//      int dist = INT_MAX; // represent max value
+
+//      for (int i = 0; i < n; i++)
+//      {
+//         if( arr[i] == x || arr[i] == y){
+          
+//           if(prev != -1 && arr[prev] != arr[i]){
+//             dist  = min(dist, i-prev);
+//           }
+//           prev = i;
+//         }
+//      }
+     
+
+//       return dist == INT_MAX ? -1 : dist;
+
+//     return 0;
+// }
+
+
+
+
+
+
+/********** Rotating an Array
+ Given an array of size N. The task is to rotate array by D elements where D ≤ N.
+   **********/
+
+// Codenation
+
+// Input: N = 7
+// Arr[] = {1, 2, 3, 4, 5, 6, 7}
+// D = 2
+// Output: 3 4 5 6 7 1 2
+// Explanation: 
+// Rotate by 1: [2, 3, 4, 5, 6, 7, 1]
+// Rotate by 2: [3, 4, 5, 6, 7, 1, 2]
+
+void reverseArr(int arr[], int low, int high){
+
+    while (low < high)
+    {
+        swap(arr[low], arr[high]);
+        low ++;
+        high--;
+    }
+    
+}
+
+
+int main(){
+     int arr[] = {1, 2, 3, 4, 5, 6, 7};
+     int n  = sizeof(arr) / sizeof(arr[0]);
+
+     int d = 3;
+      
+     reverseArr(0,n-d-1,0);
+     
+
+     return 0;
+
+}
